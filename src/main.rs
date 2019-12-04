c299f736268a48fc1ecde828336522c165af8e64
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-12-04 18:40:11 -0500

    feat: support sslmode connection parameter
    This is based on https://matthewkmayer.github.io/blag/public/post/postgres-tls/

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+extern crate url;
 extern crate env_logger;
 extern crate pg_amqp_bridge as bridge;
+extern crate openssl;
+extern crate postgres;
 extern crate r2d2;
 extern crate r2d2_postgres;
 
 use std::env;
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
+use url::{Url};
+use openssl::ssl::*;
 use r2d2::{Pool, ManageConnection};
 use r2d2_postgres::{TlsMode, PostgresConnectionManager};
 
@@ -49,9 +55,33 @@ fn main() {
 
 fn wait_for_pg_connection(pg_uri: &String) -> Pool<PostgresConnectionManager> {
   println!("Attempting to connect to PostgreSQL..");
-  let conn = PostgresConnectionManager::new(pg_uri.to_owned(), TlsMode::None).unwrap();
+
+  let parsed_url = Url::parse(env::var("POSTGRESQL_URI").ok().as_ref().unwrap()).unwrap();
+  let hash_query: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
+  let dbssl = match hash_query.get("sslmode") {
+    None => "require",
+    Some(mode) => mode
+  };
+
+  let mut builder = SslConnector::builder(::openssl::ssl::SslMethod::tls()).unwrap();
+  match dbssl.to_lowercase().as_ref() {
+      "disable" | "require" | "prefer" | "allow" => builder.set_verify(SslVerifyMode::empty()),
+      _ => (), // by default we verify certs: it's like either verify-ca or verify-full, TBD
+  }
+
+  let negotiator = Box::new(::postgres::tls::openssl::OpenSsl::new().unwrap());
+  let db_ssl_mode = match dbssl.to_lowercase().as_ref() {
+      "require" | "verify-ca" | "verify-full" => TlsMode::Require(negotiator),
+      // `disable`, `prefer`, and `allow` fall into here and will not try TLS.
+      // Not totally correct: please use at least `require` for real use.
+      _ => TlsMode::None,
+  };
+
+  let manager = PostgresConnectionManager::new(pg_uri.to_owned(), db_ssl_mode)
+    .expect("Couldn't make postgres connection manager");
+
   let mut i = 1;
-  while let Err(e) = conn.connect() {
+  while let Err(e) = manager.connect() {
     println!("{:?}", e);
     let time = Duration::from_secs(i);
     println!("Retrying the PostgreSQL connection in {:?} seconds..", time.as_secs());
@@ -60,5 +90,5 @@ fn wait_for_pg_connection(pg_uri: &String) -> Pool<PostgresConnectionManager> {
     if i > 32 { i = 1 };
   };
   println!("Connection to PostgreSQL successful");
-  Pool::new(conn).unwrap()
+  Pool::new(manager).expect("Couldn't make pool from pg connection manager")
 }
